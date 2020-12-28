1094ddd0a260f3364c7f4eebbe776474ca40a705
Oleg Yarovikov <olegy2005@gmail.com>
2020-12-28 19:56:18 +0500

    Add constructor

--- Vector/Vector/Vector.h
+++ Vector/Vector/Vector.h
@@ -1,5 +1,16 @@
 #pragma once
 class Vector
 {
+	int* elements = nullptr;
+	int size{ 0 };
+public:
+	Vector(int size) : elements{ new int[size] {} }, size{ size }
+	{
+
+	}
+
+	~Vector() {
+		delete[] elements;
+	}
 };
 
